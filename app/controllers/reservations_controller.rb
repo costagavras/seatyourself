7f87327e2e4725b6337ff119074fac55bd912d8a
StephenVarela <varelast92@gmail.com>
2018-10-11 11:30:45 -0400

    added form for with nested resource for reservations pertaining to restaurants, removed manual entry of user id and restaurant id, the new reservation will take the information from url and sessions id

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,31 +2,32 @@ class ReservationsController < ApplicationController
 
   def index
     #display all reservations pertaining to current user
-    @reservations = Reservation.all
+    @reservations = current_user.reservations.all
     #@reservations = User.find_by(id: current_user.user_id).reservations
   end
 
   def show
-    @reservation = Reservation.find_by(id: params[:id])
     @restaurant = Restaurant.find_by(id: params[:restaurant_id])
+    @reservation = Reservation.find_by(id: params[:id])
   end
 
   def new
     #display a form to create a new reservation linking a user to a resteraunt
     @reservation = Reservation.new
+    @restaurant = Restaurant.find_by(id: params[:restaurant_id])
   end
 
   def create
-
-
     @reservation = Reservation.new(date_time:  params[:reservation][:date_time],
                                    party_size: params[:reservation][:party_size],
                                    user_id: current_user.id,
                                    restaurant_id: params[:restaurant_id])
 
+    @resteraunt = Restaurant.find_by(id: params[:restaurant_id])
+
     if @reservation.save
         flash[:notice] = "Your reservation was made sucessfully"
-        redirect_to reservations_path
+        redirect_to restaurant_reservation_path(@resteraunt ,@reservation)
     else
         @reservation.errors.full_messages
         render :new
@@ -37,6 +38,7 @@ class ReservationsController < ApplicationController
   def edit
     #display form with reservation object for user to edit
     @reservation = Reservation.find_by(id: params[:id])
+    @restaurant = Restaurant.find_by(id: params[:restaurant_id])
   end
 
   def update
