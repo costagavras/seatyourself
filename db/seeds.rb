175b9e202257acb6ede21ff586a271b4ea0f98cf
StephenVarela <varelast92@gmail.com>
2018-10-11 16:35:11 -0400

    updated master with changes to application controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#wipe_user_id
 User.destroy_all
 Restaurant.destroy_all
 Neighbourhood.destroy_all
