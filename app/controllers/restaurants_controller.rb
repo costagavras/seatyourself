9ce8ba62db2d04cee2bdd860daec47681c5115db
costagavras <cgavras@yahoo.ca>
2018-10-11 14:31:22 -0400

    before migrations and setting Category controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -24,28 +24,54 @@ class RestaurantsController < ApplicationController
     @restaurant.price_range = params[:restaurant][:price_range]
     @restaurant.summary = params[:restaurant][:summary]
     @restaurant.menu = params[:restaurant][:menu]
-
-    @restaurant.neighbourhood_id = @neighbourhood.id
-    @restaurant.category_id = @category.id
+    @restaurant.neighbourhood_id = params[:restaurant][:neighbourhood_id]
+    @restaurant.category_id = params[:restaurant][:category_id]
     if @restaurant.save
       # if the picture gets saved, generate a get request to "/pictures" (the index)
+      flash[:notice] = "Restaurant added!"
       redirect_to "/restaurants"
     else
       # otherwise render new.html.erb
+      flash[:alert] = "Restaurant not added!"
       render :new
     end
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
 
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.capacity = params[:restaurant][:capacity]
+    @restaurant.picture_url = params[:restaurant][:picture_url]
+    @restaurant.opening_hours = params[:restaurant][:opening_hours]
+    @restaurant.closing_hours = params[:restaurant][:closing_hours]
+    @restaurant.address = params[:restaurant][:address]
+    @restaurant.price_range = params[:restaurant][:price_range]
+    @restaurant.summary = params[:restaurant][:summary]
+    @restaurant.menu = params[:restaurant][:menu]
+    @restaurant.neighbourhood_id = params[:restaurant][:neighbourhood_id]
+    @restaurant.category_id = params[:restaurant][:category_id]
+
+
+    if @restaurant.save
+      flash[:notice] = "Restaurant updated !"
+      redirect_to "/restaurants/#{@restaurant.id}"
+     else
+      render :edit
+    end
   end
 
   def edit
-
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def destroy
-
+    @restaurant = Product.find(params[:id])
+    @restaurant.destroy
+    if @restaurant.destroy
+      flash[:notice] = "Restaurand deleted !"
+      redirect_to restaurants_path
+    end
   end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,15 @@ class Restaurant < ApplicationRecord
   belongs_to :category
   belongs_to :neighbourhood
   has_and_belongs_to_many :users
+
+validates :name, presence: { message: "field must be filled out!" }
+validates :summary, presence: { message: "field must be filled out!" }
+validates :address, presence: { message: "field must be filled out!" }
+validates :category_id, inclusion: ["it", "me", "as", "cf"]
+validates :neighbourhood_id, inclusion: ["1", "2", "3", "4"]
+validates :menu, presence: { message: "field must be filled out!" }
+validates :picture_url, presence: { message: "field must be filled out!" }
+validates :capacity, numericality: {only_integer: true}, presence: { message: "field must be filled out!" }
+validates :price_range, inclusion: ["1", "2", "3"]
+
 end
