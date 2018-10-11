dd322fd87b08928d6f5dce183f1454df4c2f42bf
costagavras <cgavras@yahoo.ca>
2018-10-11 11:49:57 -0400

    3 params filter on index page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    @my_restaurants = Restaurant.where(:price_range => params[:price_range], :neighbourhood_id => params[:neighbourhood],:category_id => params[:category])
   end
 
   def new
@@ -12,7 +13,27 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new
 
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.capacity = params[:restaurant][:capacity]
+    @restaurant.picture_url = params[:restaurant][:picture_url]
+    @restaurant.opening_hours = params[:restaurant][:opening_hours]
+    @restaurant.closing_hours = params[:restaurant][:closing_hours]
+    @restaurant.address = params[:restaurant][:address]
+    @restaurant.price_range = params[:restaurant][:price_range]
+    @restaurant.summary = params[:restaurant][:summary]
+    @restaurant.menu = params[:restaurant][:menu]
+
+    @restaurant.neighbourhood_id = @neighbourhood.id
+    @restaurant.category_id = @category.id
+    if @restaurant.save
+      # if the picture gets saved, generate a get request to "/pictures" (the index)
+      redirect_to "/restaurants"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end
   end
 
   def update
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,10 +43,10 @@ opening_hours: 12,
 closing_hours: 2,
 address: "42 Tagliatelle str.",
 price_range: 2,
-neighbourhood_id: Neighbourhood.first.id,
+neighbourhood_id: 1,
 summary: "Your italian food from la nonna",
 menu: "Ipse lorem",
-category_id: Category.first.id,
+category_id: "it",
 name: "Little Nero",
 picture_url: "https://res.cloudinary.com/simpleview/image/upload/crm/napavalley/HR-azzurro-exterior-credit-Mario-Piombo-b1dd5bb85056a36_88281807-5056-a36a-08a3b32db2fe41f8.jpg")
 
