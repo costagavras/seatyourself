1dfe68a724ae05ab62201505b905cc53c28f8539
StephenVarela <varelast92@gmail.com>
2018-10-10 23:14:32 -0400

    added reservation views and seeds to test

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,11 +2,12 @@ class ReservationsController < ApplicationController
 
   def index
     #display all reservations pertaining to current user
-    @reservations = User.find_by(id: current_user.user_id).reservations
+    @reservations = Reservation.all
+    #@reservations = User.find_by(id: current_user.user_id).reservations
   end
 
   def show
-    @reservation = Reservation.find_by(:id params[:id])
+    @reservation = Reservation.find_by(id: params[:id])
   end
 
   def new
@@ -16,10 +17,10 @@ class ReservationsController < ApplicationController
 
   def create
     #create the new reservation based on user input
-    @reservation = Reservation.new(datetime: =  params[:reservation][:datetime],
-                                   party_size: =  params[:reservation][:party_size],
-                                   user_id: =  params[:user_id],
-                                   restaurant_id: = params[:restaurant_id])
+    # @reservation = Reservation.new(datetime: =  params[:reservation][:datetime],
+    #                                party_size: =  params[:reservation][:party_size],
+    #                                user_id: =  params[:user_id],
+    #                                restaurant_id: = params[:restaurant_id])
 
     if @reservation.save
         flash[:notice] = "Your reservation was made sucessfully"
@@ -33,14 +34,14 @@ class ReservationsController < ApplicationController
 
   def edit
     #display form with reservation object for user to edit
-    @reservation = Reservation.find_by(id: params[:id])
+    # @reservation = Reservation.find_by(id: params[:id])
   end
 
   def update
-    @reservation = Reservation.new(datetime: =  params[:reservation][:datetime],
-                                   party_size: =  params[:reservation][:party_size],
-                                   user_id: =  params[:user_id],
-                                   restaurant_id: = params[:restaurant_id])
+    # @reservation = Reservation.new(datetime: =  params[:reservation][:datetime],
+    #                                party_size: =  params[:reservation][:party_size],
+    #                                user_id: =  params[:user_id],
+    #                                restaurant_id: = params[:restaurant_id])
     if @reservation.save
       flash[:notice] = "Your reservation was made sucessfully"
                                        redirect_to resteraunt_path
@@ -51,7 +52,7 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
-    @reservation = Reservation.find_by(id: params[:id])
+    # @reservation = Reservation.find_by(id: params[:id])
 
     if @reservation.destroy
       flash[:notice] = "Your reservation was sucessfully cancelled"
